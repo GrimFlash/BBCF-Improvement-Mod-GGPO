9e12e2fd8117c197e60d58a359b69b15099d9cd1
GrimFlash <65523164+GrimFlash@users.noreply.github.com>
2021-01-17 15:02:07 -0500

    Add keyboard input for saving/loading gamestate
    When the checkbox on the Save/Load state menu is enabled, F8 can be used to save state and F9 can be used to load state

--- src/Overlay/Window/SaveLoadWindow.cpp
+++ src/Overlay/Window/SaveLoadWindow.cpp
@@ -13,8 +13,10 @@ void SaveLoadWindow::Draw()
 
 void SaveLoadWindow::DrawSaveLoadStateWindow() 
 {
-	if (!isSaveLoadStateEnabledInCurrentState())
+	if (!isSaveLoadStateEnabledInCurrentState()) {
+		ImGui::Text("NOT IN VERSUS/TRAINING");
 		return;
+	}
 	
 	const int nMinSkip = 0;
 	const int nMaxSkip = 60;
@@ -22,28 +24,44 @@ void SaveLoadWindow::DrawSaveLoadStateWindow()
 	static int nFrameStep = 1;
 	static SavedGameState gameState = {};
 
+	static bool isTrue = false;
+
 	ImGui::SliderInt("Num of frames to skip", &nFramesToSkipRender, nMinSkip, nMaxSkip);
 
 	ImGui::VerticalSpacing(10);
 
-	if (ImGui::Button("Save", ImVec2(50, 25))) {
+	if (ImGui::Button("Save", ImVec2(50, 25))) 
 		saveGameState(gameState);
-
-		//savedState = SaveGameState();
-	}
 
 	ImGui::SameLine();
 
 	if (ImGui::Button("Load", ImVec2(50, 25))) {
-		loadGameState(gameState);
-
-		//LoadGameState(savedState);
 		/*
 		lpGameState->nFramesSkipped = 0;
 		lpGameState->nFramesToSkipRender = nFramesToSkipRender;
-		LoadGameState(lpGameState, &savedState);
 		*/
+		loadGameState(gameState);
 	}
+
+	ImGui::Checkbox("Enable Keyboard input", &isTrue);
+
+	if (isTrue) {
+		ImGui::Text("Press F8 to save\nPress F9 to load");
+
+		if (ImGui::IsKeyPressed(119, false)) { // F8 will save state
+			saveGameState(gameState);
+		}
+
+		if (ImGui::IsKeyPressed(120, false)) { // F9 will load state
+			/*
+			lpGameState->nFramesSkipped = 0;
+			lpGameState->nFramesToSkipRender = nFramesToSkipRender;
+			*/
+			loadGameState(gameState);
+		}
+	}
+
+	
 
 	/*This will be the workspace for getting the save/load state functioning*/
 }
